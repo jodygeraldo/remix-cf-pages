2300eb4a9d28a774ab6c002776392403eeb896ab
Jody Geraldo <jody.geraldo.jg@gmail.com>
2022-10-17 11:10:29 +0700

    chore: format

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,6 @@
 import { expect, test } from '@playwright/test'
 
-test('homepage has Remix in title', async ({
-  page,
-}) => {
+test('homepage has Remix in title', async ({ page }) => {
   await page.goto('localhost:8788')
 
   // Expect a title "to contain" a substring.
